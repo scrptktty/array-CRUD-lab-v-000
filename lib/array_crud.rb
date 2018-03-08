a5598e5ec02073929497c996d3911904c61e6acc
Kachel Wittig <kjwittig@gmail.com>
2018-03-08 01:50:24 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [a, b, c, d]
+  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  arr2 = 
+  arr2 =
 end
 
 def add_element_to_start_of_array(array, element)
